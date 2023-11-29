8f6cfef0e4525bd5e532d7a4afaa53e12e11b2d5
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-11-29 21:50:44 +0300

    commit

--- src/main/java/de/telran/firstspringproject3/HelloController.java
+++ src/main/java/de/telran/firstspringproject3/HelloController.java
@@ -1,6 +1,10 @@
 package de.telran.firstspringproject3;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -29,5 +33,34 @@ public class HelloController {
     public List<Book> show() {
         return books;
     }
+
+    @GetMapping(value = "/book/{id}")
+    public Book getBook(@PathVariable(value = "id") Integer id) {
+        return books.get(id);
+    }
+
+
+    @GetMapping(value = "/calculator")
+    public Integer calculate(@RequestParam(name = "first") Integer a, @RequestParam(name = "second") Integer b) {
+        return a + b;
+    }
+
+    @GetMapping(value = "/what-is-your-name")
+    public String sayYourName(@RequestParam(name = "name") String name, @RequestParam(name = "lang") String lang) {
+        if (lang.equals("eng")) {
+            return "Hello, " + name;
+        }
+        if (lang.equals("rus")) {
+            return "Привет, " + name;
+        }
+
+        return "Привет всем";
+    }
+
+    @PostMapping(value = "/book/add")
+    public void addNewBook(@RequestBody Book book) {
+        books.add(book);
+    }
+
 
 }
